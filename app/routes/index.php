c775e059d4033f40e159d78125c6f12f05491bf0
Andres Felipe Martinez Cordero <andrepipemc@gmail.com>
2013-10-13 00:22:22 -0500

    Update index.php

--- app/routes/index.php
+++ app/routes/index.php
@@ -1,6 +1,8 @@
 <?php
 
+//Router Index Application
 $app->get('/', function () use ($app){
+	
 	$app->view->setData(array('title' => 'Index'));
-    $app->render('index.php');
+    	$app->render('index.php');
 });
